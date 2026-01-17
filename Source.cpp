3ae22c0633fb5746db20fb1d541543e3af508425
alexiajurjut <alexia.jurjut@gmail.com>
2026-01-18 00:21:52 +0200

    Add files via upload
    Commit 1

---
+++ Source.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <ctime>
+
+using namespace std;
+
+struct Book {
+    string title;
+    string author;
+    string isbn;
+    string category;
+    string year;
+    string borrowed_by = "";
+    int borrow_count = 0;
+    long long due_ts = 0;
+};
+
+struct User {
+    string name;
+    string id;
+    string email;
+    int history_count = 0;
+};
+
+vector<Book> g_books;
+vector<User> g_users;
+
+const string BOOKS_FILE = "books.csv";
+const string USERS_FILE = "users.csv";
+
+int main() {
+    return 0;
+}
